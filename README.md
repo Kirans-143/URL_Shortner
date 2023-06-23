04a582c9d2aa0d03e4cebe9faaa06fb95013173d
Kirans-143 <kiran.shet77@gmail.com>
2023-06-23 14:04:09 +0530

    Front end with backend integration

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Routes
 # GET/:id - Redirects the user to the original URL
 
 # GET/URL/analytics/:id - Returns the clicks for the provided short id
+
+# npm i ejs for server side rendering
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const staticRoute = require("./routes/static.router");
 const urlRoute = require("./routes/url.router");
 const URL = require("./models/url.model");
 const { connectMongoDB } = require("./config/db.config");
@@ -15,9 +18,22 @@ connectMongoDB("mongodb://localhost:27017/short-url")
     console.log("Error while connecting");
   });
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  });
+});
+
 app.use("/url", urlRoute);
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRoute);
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
